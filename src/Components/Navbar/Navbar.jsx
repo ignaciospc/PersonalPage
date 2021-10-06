710eb192b92e99335d0cfdba7f7c4d24dafbc904
ignaciospc <molinaclari@Air-de-Maria.fibertel.com.ar>
2021-10-05 21:31:38 -0300

    se continua trabajando con el toggler del navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,13 +4,36 @@ import './Navbar.css'
 function Navbar () {
     return(
         <div>
+            <div className='container'>
             <nav className='nav-css navbar navbar-ligth '>
                 <a href='#' className='nav-item nav-link active'>
-                    <img src='images/sunflower-icon.png' width='60' height='50'/>
+                    <img src='images/sunflower-icon.png' width='60' height='50' />
                     <span className='nav-name'>Clara Molina</span>
                 </a>
+
+                <button className='navbar-toggler' type='button' >
+
+                </button>
+
+                <ul className='nav justify-content-end'>
+                    <li className='nav-item'>
+                        <a className='nav-link a-color' href="">About me</a>
+                    </li>
+                    <li className='nav-item'>
+                        <a className='nav-link a-color' href="">Experence</a>
+                    </li>
+                    <li className='nav-item'>
+                        <a className='nav-link a-color' href="">Contact</a>
+                    </li>
+                </ul>
             </nav>
+            </div>
         </div>
+
+            
+            
+
+       
     )
 }
 
